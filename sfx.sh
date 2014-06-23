11bdeea96d09bb0fed1445fdd66bd8d8e1448342
access2godzilla <access2godzilla@gmail.com>
2014-06-23 22:19:26 +0530

    Add checksum support, remove superflous code

--- sfx.sh
+++ sfx.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echoerr ()
-{
-	echo "$1" >& 2
-}
+
+# Handle the verification/extraction of the installer
+
+unset CDPATH
+
+if [[ ! "$(sed -r s/[a-f0-9]{64}// "$0" | sha256sum)" =~ __CHECKSUM__ ]]; then
+	echo "The installer is damaged!"
+	exit 2
+fi
 
 case $(uname -m) in
 i?86)
@@ -12,16 +17,13 @@ x86_64)
 	mtype=x86_64
 	;;
 *)
-	echoerr "Unsupported architecture."
+	echo "Unsupported architecture."
 	exit 1
 	;;
 esac
 
 installer_dir=$(mktemp -d /tmp/pminstaller.XXXXXX)
-if ! tail -n +__LINENUM__ "$0" | tar -xJf - -C "$installer_dir"; then
-	echoerr "Self-extraction failure!"
-	exit 2
-fi
+tail -n +__LINENUM__ "$0" | tar -xJf - -C "$installer_dir" || exit 2
 
 PATH="$installer_dir/bin/$mtype:$installer_dir/tools:$PATH"
 runasroot "$installer_dir/installer.sh"
