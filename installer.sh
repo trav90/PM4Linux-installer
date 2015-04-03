f04452f99851fbee04b08d7bc45b6d6d34f3a7d8
Moonchild <git-repo@palemoon.org>
2015-04-04 01:01:34 +0200

    Fix typo

--- installer.sh
+++ installer.sh
@@ -344,7 +344,7 @@ if ! mklock; then
 fi
 
 # Processor check
-if ! grep sse2 /proc/cpuinfo >dev/null; then
+if ! grep sse2 /proc/cpuinfo >/dev/null; then
 	dlg_e "Pale Moon requires a processor that supports the SSE2 instruction set."
 fi
 
